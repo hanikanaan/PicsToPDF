c349d6876092b3b6129f66f65e6a05b8525590a8
hanikanaan <58312140+hanikanaan@users.noreply.github.com>
2021-10-09 10:59:25 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+from PIL import Image
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+
+root = tk.Tk()
+
+canvas1 = tk.Canvas(root, width=300, height=300, bg='white', relief='raised')
+canvas1.pack()
+
+label1 = tk.Label(root, text='File Conversion Tool', bg='white')
+label1.config(font=('helvetica', 20))
+canvas1.create_window(150, 60, window=label1)
+
+imagelist = [
+
+]
+
+
+def getFile():
+    global im1
+
+    import_file_path = filedialog.askopenfilename()
+    image1 = Image.open(import_file_path)
+    im1 = image1.convert('RGB')
+    msgBox = tk.messagebox.askquestion('Exit Application?', 'Would you like to scan more pictures?',
+                                       icon='question')
+
+    while msgBox == 'yes':
+        import_file_path = filedialog.askopenfilename()
+        image = Image.open(import_file_path)
+        im = image.convert('RGB')
+        imagelist.append(im)
+        msgBox = tk.messagebox.askquestion('Exit Application?', 'Would you like to scan more pictures?',
+                                           icon='question')
+        if msgBox == 'no':
+            break
+
+
+browseButton = tk.Button(text="     Select File     ", command=getFile, bg='green', fg='white',
+                         font=('helvetica', 12, 'bold'))
+canvas1.create_window(150, 130, window=browseButton)
+
+
+def convertToPdf():
+
+    export_file_path = filedialog.asksaveasfilename(defaultextension='.pdf')
+    im1.save(export_file_path, save_all=True, append_images=imagelist)
+
+
+saveAsButton = tk.Button(text='Convert to PDF', command=convertToPdf, bg='green', fg='white',
+                         font=('helvetica', 12, 'bold'))
+canvas1.create_window(150, 180, window=saveAsButton)
+
+
+def exitApplication():
+    MsgBox = tk.messagebox.askquestion('Exit Application', 'Are you sure you want to exit the application',
+                                       icon='warning')
+    if MsgBox == 'yes':
+        root.destroy()
+
+
+exitButton = tk.Button(root, text='Exit Application', command=exitApplication, bg='brown', fg='white',
+                       font=('helvetica', 12, 'bold'))
+canvas1.create_window(150, 230, window=exitButton)
+
+root.mainloop()
